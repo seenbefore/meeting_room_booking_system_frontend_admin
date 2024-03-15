e77b247d47b98b6cba7619f72eab80678604ab00
Jason T <11312673+seenbefore@user.noreply.gitee.com>
2024-03-15 16:20:05 +0800

    修改

--- src/interfaces/interfaces.ts
+++ src/interfaces/interfaces.ts
@@ -8,7 +8,7 @@ import { SearchBooking } from "../pages/BookingManage/BookingManage";
 import dayjs from "dayjs";
 
 const axiosInstance = axios.create({
-    baseURL: 'http://121.40.76.186/api',
+    baseURL: 'http://120.26.63.71/api',
     timeout: 3000
 });
 
